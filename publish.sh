6c0f774720b8973baf0999785837f0e0e8382394
qifei_Dev <lqf1024@antbim.com>
2020-09-02 15:39:27 +0800

    code

--- publish.sh
+++ publish.sh
@@ -4,6 +4,9 @@ npm config set registry=https://registry.npmjs.org
 npm login
 echo '---------publishing--------'
 npm publish
-npm config set registry=https://registry.npm.taobao.org
+
 echo '---------publishing end--------'
 exit
+
+# npm config set registry=https://registry.npm.taobao.org
+
